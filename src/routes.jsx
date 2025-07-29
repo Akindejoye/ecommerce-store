6e940074416d9cce5c43b85c60c2c0829323844e
Akindejoye <tydruhill@gmail.com>
2025-07-29 20:38:40 +0100

    Updating Route

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import ProductDetails from "./pages/ProductDetails";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
 
 const router = createBrowserRouter([
   {
@@ -19,6 +21,24 @@ const router = createBrowserRouter([
       <>
         <Navbar />
         <ProductDetails />
+      </>
+    ),
+  },
+  {
+    path: "/cart",
+    element: (
+      <>
+        <Navbar />
+        <Cart />
+      </>
+    ),
+  },
+  {
+    path: "/login",
+    element: (
+      <>
+        <Navbar />
+        <Login />
       </>
     ),
   },
