12d7b38a518b8376e342621af937aec0c482aaf7
Akindejoye <tydruhill@gmail.com>
2025-08-20 00:42:02 +0100

    Fixing query bug

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,9 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useContext, useEffect, useState } from "react";
 import { getProductsByQuery } from "../api/api";
 import { Link, useSearchParams } from "react-router-dom";
 import ProductCard from "../components/ProductCard";
 import "../styles/home.css";
+import { CartContext } from "../context/CartContext";
 
 function Home() {
   const [products, setProducts] = useState([]);
@@ -12,6 +13,7 @@ function Home() {
   const [category, setCategory] = useState("All");
   const [searchParams, setSearchParams] = useSearchParams();
   const [isSearching, setIsSearching] = useState(false);
+  const { addToCart } = useContext(CartContext);
 
   // Fetch Products
   const fetchProducts = useCallback(async (query, filterType) => {
@@ -43,15 +45,16 @@ function Home() {
     function () {
       const query = searchParams.get("q") || ""; // Read q param
       const cat = searchParams.get("category") || "All"; // Read category param
-      setSearchQuery(query);
-      setCategory(cat);
-      setIsSearching(!!query);
-      if (query) {
-        fetchProducts(query, "search");
-      } else if (cat !== "All") {
-        fetchProducts(cat, "category");
-      } else {
-        fetchProducts("", "category");
+      if (query !== searchQuery || cat !== category) {
+        // Skip if params match state
+        setSearchQuery(query);
+        setCategory(cat);
+        setIsSearching(!!query);
+        if (query) {
+          fetchProducts(query, "search");
+        } else {
+          fetchProducts(cat, "category");
+        }
       }
     },
     [fetchProducts, searchParams]
@@ -59,11 +62,13 @@ function Home() {
 
   // Handle category
   useEffect(() => {
-    if (isSearching) return; // skip category fetch during search
-    setSearchQuery(""); // Reset search when changing category
+    if (isSearching) {
+      return;
+    }
     fetchProducts(category, "category");
-    setSearchParams(category === "All" ? {} : { category }); // update URL with category param
-  }, [category, fetchProducts, setSearchParams]);
+    setSearchQuery(""); // Reset search when changing category
+    setSearchParams(category === "All" ? {} : { category }, { replace: true }); // update URL with category param. Use replace to avoid extra history entry
+  }, [category, fetchProducts, isSearching, setSearchParams]);
 
   // Handle search on Enter
   const handleSearchSubmit = (e) => {
@@ -71,7 +76,7 @@ function Home() {
       setIsSearching(true);
       setCategory("All"); // Reset category on search
       fetchProducts(searchQuery, "search");
-      setSearchParams({ q: searchQuery }); // Update URL with q param
+      setSearchParams({ q: searchQuery }, { replace: true }); // Update URL with q param
       setSearchQuery(""); // Clear input after search
     }
   };
@@ -95,7 +100,7 @@ function Home() {
           value={category}
           onChange={(e) => {
             setCategory(e.target.value);
-            setIsSearching(false); // reset is Searching on category change
+            setIsSearching(false); // CHANGED: Reset isSearching on category change
           }}>
           <option value="All">All Categories</option>
           <option value="Books">Books</option>
